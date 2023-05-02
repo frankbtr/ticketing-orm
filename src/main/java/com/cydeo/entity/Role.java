48d7554d602db0f3c9c16171e7ef1d84f5369f23
Frank Batur <frank_batur@outlook.com>
2023-05-01 21:16:38 -0400

    wip

--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -3,12 +3,17 @@ package com.cydeo.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Role {
-    private Long id;
+@Table(name="roles")
+@Entity
+public class Role extends BaseEntity {
     private String description;
-
 }
